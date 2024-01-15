18da69006999f00cb2553ffdc071ef1b673ca17a
David Jones <d.t.jones@outlook.com>
2024-01-15 16:13:48 -0500

    toml fixes

--- tsd/TextSpottingDataset.py
+++ tsd/TextSpottingDataset.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 from pathlib import Path
 from PIL import Image, ImageDraw, ImageFont
-from TextAnnotation import TextAnnotation, BoxAnnotation, DotAnnotation, QuadAnnotation
+from TextAnnotation import TextAnnotation, DotAnnotation
 
 COLORS = [
     "#ea5545", 
@@ -125,109 +125,3 @@ class TextSpottingDataset(ABC):
             self._build_training()
 
 
-class ICDAR2013(TextSpottingDataset):
-    # Scene text dataset containing most of the well-captured and horizontal
-    # texts, annotated in boundng box formats.
-    # [Source](https://iapr.org/archives/icdar2013/). 
-    # Also see: https://rrc.cvc.uab.es/?ch=2&com=downloads
-    # 1. 229 training images
-    # 2. 233 test images
-    def __init__(self, root: str|Path, mode: str="train"):
-        super().__init__(root, mode)
-        self.name = "ICDAR2013"
-        self.train_images_path = self.root / "Challenge2_Training_Task12_Images"
-        self.train_labels_path = self.root / "Challenge2_Training_Task1_GT"
-        
-        self.test_images_path = self.root / "Challenge2_Test_Task12_Images"
-        self.test_labels = self.root / "Challenge2_Test_Task1_GT"
-
-    def _verify_structure(self):
-        for d in [self.train_images_path, self.train_labels_path, 
-                  self.test_images_path, self.test_labels]:
-            if not d.exists():
-                raise FileNotFoundError(f"Couldn't find {d.resolve()} for "
-                                        f"{self.name}, check folder structure.")
-        return True
-    
-    def _build_training(self):
-        image_paths = []
-        annotations = []
-        for train_img_path in self.images.glob("*.jpg"):
-            anno = []
-            gt_fname = ("gt_"+train_img_path.stem+".txt")
-            train_label_path = self.labels / gt_fname
-            
-            with open(train_label_path, 'r') as f:
-                for line in f.readlines():
-                    left, top, right, bottom, text = line.strip().split(" ")
-                    anno.append(
-                        BoxAnnotation(text.replace("\"", ""), 
-                                      int(left), 
-                                      int(top), 
-                                      int(right), 
-                                      int(bottom)))
-            image_paths.append(train_img_path)
-            annotations.append(anno)
-        
-        self.image_paths = image_paths
-        self.annotations = annotations
-
-    def __len__(self) -> int:
-        return len(self.image_paths)
-    
-    def __getitem__(self, idx: int) -> tuple[str, list[TextAnnotation]]:
-        return TextSpottingDatasetElement(
-            self.image_paths[idx], 
-            self.annotations[idx])
-
-class ICDAR2015(ICDAR2013):
-    # A scene text dataset containing inclined texts. Annotated using
-    # quadrilateral format. [Source](https://rrc.cvc.uab.es/?ch=4).
-    # Also see: https://rrc.cvc.uab.es/?ch=2&com=downloads
-    # 1. 1000 training images
-    # 2. 500 test images
-    def __init__(self, root: str|Path, mode: str="train"):
-        super().__init__(root, mode)
-        self.name = "ICDAR2015"
-        self.train_images_path = self.root / "ch4_training_images"
-        self.train_labels_path = self.root / "ch4_training_localization_transcription_gt"
-        
-        self.test_images_path = self.root / "ch4_test_images"
-        self.test_labels = self.root / "Challenge4_Test_Task1_GT"
-        
-    def _build_training(self):
-        image_paths = []
-        annotations = []
-        for train_img_path in self.images.glob("*.jpg"):
-            anno = []
-            gt_fname = ("gt_"+train_img_path.stem+".txt")
-            train_label_path = self.labels / gt_fname
-            
-            with open(train_label_path, 'r') as f:
-                for line in f.readlines():
-                    line_data = line.strip().split(",")
-                    x1, y1, x2, y2, x3, y3, x4, y4 = line_data[:8]
-                    
-                    # Sometimes there is a commma at the end of the word 
-                    text = "".join(line_data[8:])
-                    anno.append(
-                        QuadAnnotation(
-                            text, 
-                            int(x1), int(y1), 
-                            int(x2), int(y2), 
-                            int(x3), int(y3), 
-                            int(x4), int(y4)))
-            image_paths.append(train_img_path)
-            annotations.append(anno)
-        
-        self.image_paths = image_paths
-        self.annotations = annotations
-        
-
-if __name__ == "__main__":
-    import code
-    d = ICDAR2013("../sources/icdar2013")
-    d.build()
-    d[0].visualize('test.png')
-        
-    code.interact(local=locals())
